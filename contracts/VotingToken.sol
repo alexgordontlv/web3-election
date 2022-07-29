6226a3424ce9d7c44ea31dc1bb7fe3f6b6d97c68
alexgordontlv <alexgordon.tlv@gmail.com>
2022-07-29 09:37:42 +0300

    addedadded voter

--- contracts/VotingToken.sol
+++ contracts/VotingToken.sol
@@ -1,20 +1,22 @@
-pragma solidity ^0.8.7;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.10;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // This contract is used to award the voters in our election with our precious VTN.
 
-contract votingToken is ERC20{
+contract votingToken is ERC20 {
     address public admin;
-    constructor() ERC20 ('Voting Token', 'VTN'){
-        _mint(msg.sender, 10000 * 10 **18);
+
+    constructor() ERC20("Voting Token", "VTN") {
+        _mint(msg.sender, 10000 * 10**18);
         admin = msg.sender;
     }
-    function mint(address to, uint amount) external{
-        require (msg.sender == admin, 'Only admin can mint tokens');
-        _mint (to, amount);
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, "Only admin can mint tokens");
+        _mint(to, amount);
     }
 
-    function burn (uint amount) external {
+    function burn(uint256 amount) external {
         _burn(msg.sender, amount);
     }
 }
